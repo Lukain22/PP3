4a3dc8c025e660b848567b198a0d47fa250e5c59
Luciano Tarizzo <ltarizzo91@gmail.com>
2026-04-24 00:14:53 -0300

    Validaciones de login. Ruteo login > dashboard. validación campo login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 // Importación del FormBuilder para crear formularios reactivos
-import { FormBuilder } from '@angular/forms';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +12,30 @@ export class LoginComponent {
 
 // Formulario reactivo para el login con campos de usuario y contraseña
 loginForm = this.formBuilder.group({
-  usuario: ['judaspriest@gmail.com'], // Campo para el nombre de usuario
-  contraseña: ['']  // Campo para la contraseña
+  usuario: ['judaspriest@gmail.com', [Validators.required, Validators.email]], // Campo para el nombre de usuario
+  contraseña: ['', Validators.required]  // Campo para la contraseña
 });
 
   // Constructor para inyectar el FormBuilder
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
-}
+  //
+  ngOnInit(): void {
+  }
 
+// Método para manejar el evento de inicio de sesión
+login() {
+
+  if(this.loginForm.valid) {
+      console.log("Llamar al servicio de login");
+      this.router.navigate(['/dashboard']);
+      this.loginForm.reset(); // Limpiar el formulario después de iniciar sesión  
+
+  }
+
+  else{
+    this.loginForm.markAllAsTouched(); // Marcar todos los campos como tocados para mostrar los errores de validación
+    alert("Error al intentar iniciar sesión, formulario no válido");
+  }
+
+       }}
